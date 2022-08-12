963521d7a156e6b2fe6a82f493c1a99d6ef2a09e
Jan Parisek <janparisek93@gmail.com>
2022-08-12 02:16:42 +0200

    Refactor default function to run asynchronously

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -5,17 +5,15 @@ import { REST } from '@discordjs/rest'
 // Internal dependencies
 import registeredInteractions from './interactions/index.js'
 
-export default function (clientId: string, token: string): void {
-  const commands = registeredInteractions.map((command) =>
-    command.data
-  )
+export default async function (clientId: string, token: string): Promise<void> {
+  const commands = registeredInteractions.map((command) => command.data)
 
-  const rest = new REST({ version: "10" }).setToken(token);
+  const rest = new REST({ version: '10' }).setToken(token)
 
-  rest
+  await rest
     .put(Routes.applicationCommands(clientId), { body: commands })
-    .then(() => console.log("Successfully registered application commands."))
-    .catch(error => {
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch((error) => {
       console.error('Could not register application commands.')
       console.error(error)
     })
